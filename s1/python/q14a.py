442b2fc6f2a650b6d3286103a922e6c1dc48acf8
Harishanker <harishanker.premkumar@gmail.com>
2023-10-13 14:11:28 +0530

    Python questions updated

--- s1/python/q14a.py
+++ s1/python/q14a.py
@@ -1,1 +1,3 @@
 # Q. Find palindrome without using if 
+str=input("enter a string:")
+print("Palindrome:{}".format(bool(str.lower().strip()==str[::-1].lower().strip())))
--- s1/python/q20.py
+++ s1/python/q20.py
@@ -4,13 +4,14 @@ numbers=[]
 for _ in range(limit):
     number=int(input("Enter a number: "))
     numbers.append(number)
-
-for i in range(len(numbers)):
-    for j in range(i+1,len(numbers)):
-        if(numbers[i]>numbers[j]):
-            temp=numbers[i]
-            numbers[i]=numbers[j]
-            numbers[j]=temp
+numbers.sort()
+numbers=list(set(numbers))
+# for i in range(len(numbers)):
+#     for j in range(i+1,len(numbers)):
+#         if(numbers[i]>numbers[j]):
+#             temp=numbers[i]
+#             numbers[i]=numbers[j]
+#             numbers[j]=temp
 
 
 print("The second smallest number is ",numbers[1])
