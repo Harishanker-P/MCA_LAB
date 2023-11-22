0867c74c693f8360cdd0bac764e8dee2d8fdfeac
Harishanker <harishanker.premkumar@gmail.com>
2023-11-22 11:51:08 +0530

    wpl web -started database connection code

---
+++ s1/WPL/php/dbTest/insert.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "","S3MCA");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Student(id,name)values(2,'ajay')";
+
+if ($conn->query($sql) === TRUE) {
+echo "New record created successfully";
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ s1/WPL/php/dbTest/readme.txt
@@ -0,0 +1,9 @@
+go to browser and type localhost 
+now select the phpmyadmin option
+
+if any error you will have to restart the mysql server by 
+$ sudo /etc/init.d/mysql stop
+$ sudo /opt/lampp/lampp restart
+
+
+now reload the page 
