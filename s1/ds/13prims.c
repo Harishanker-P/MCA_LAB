fde80fe0fa1a0e348460d6589e3944fe31b87cc4
Harishanker <harishanker.premkumar@gmail.com>
2023-11-23 11:47:55 +0530

    prims started....

---
+++ s1/ds/13prims.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int v[10], near[10], cost[50][50] = {999};
+
+int main()
+{
+    int vn;
+    printf("Enter the number of verrices: ");
+    scanf("%d", &vn);
+   
+
+    return 0;
+}
